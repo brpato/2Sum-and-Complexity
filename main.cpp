307c3e645cc4777a53a95a2bc64be9a9622eab22
brpato <bryanhuynh84@yahoo.com>
2026-09-04 17:23:30 -0700

    added no solution case

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ std::pair<int, int> twoSumBruteForce(const std::vector<int>& nums, int target){
             }
         }
     }
-    return {};
+    return {-1, -1};
 }
 
 std::pair<int, int> twoSumHash(const std::vector<int>& nums, int target){
@@ -25,38 +25,41 @@ std::pair<int, int> twoSumHash(const std::vector<int>& nums, int target){
             map[nums[i]] = i;
         }
     }
-    return {};
+    return {-1, -1};
 }
 
 int main() {
     std::vector<std::vector<int>> tests = {{15, 4, 18, 8, 19, 22, 24, 59, 20, 18, 12, 36, 42, 9},
-    {1, 2, 3},
+    {1, 5, 3},
     {2, 4, 8, 16},
-    {10, 9, 8, 7},
+    {9, 10, 7, 8},
     {0, 11, 0, 33, 44}};
-    std::vector<int> tars = {24, 5, 18, 17, 44};
-
+    std::vector<int> tars = {24, 5, 18, 17, 0};
     std::string bValid;
     std::string hValid;
 
-    for(int i = 0; i < static_cast<int>(tests.size()); i++){
+    for(int i = 0; i < (tests.size()); i++) {
         std::pair<int, int> bRes = twoSumBruteForce(tests[i], tars[i]);
         std::pair<int, int> hRes = twoSumHash(tests[i], tars[i]);
-
-        if(tests[i][bRes.first] + tests[i][bRes.second] == tars[i]){
-            bValid = "= " + std::to_string(tars[i]) + "? valid";
+        std::cout<< "test case " << i + 1 << "\n";
+        if(bRes.first == -1 or hRes.first == -1){
+            std::cout<< "no solution exists\n\n";
         }
         else{
-            bValid = "= " + std::to_string(tars[i]) + "? invalid";
-        }
+            if(tests[i][bRes.first] + tests[i][bRes.second] == tars[i]){
+                bValid = "= " + std::to_string(tars[i]) + "? valid";
+            }
+            else{
+                bValid = "= " + std::to_string(tars[i]) + "? invalid";
+            }
 
-        if(tests[i][hRes.first] + tests[i][hRes.second] == tars[i]){
-            hValid = "= " + std::to_string(tars[i]) + "? valid";
+            if(tests[i][hRes.first] + tests[i][hRes.second] == tars[i]){
+                hValid = "= " + std::to_string(tars[i]) + "? valid";
+            }
+            else{
+                hValid = "= " + std::to_string(tars[i]) + "? invalid";
+            }
+            std::cout<< "bruteforce\n[" << bRes.first << ", " << bRes.second << "]\n" << tests[i][bRes.first] << ", " << tests[i][bRes.second] << "\n" << bValid << "\nhashmap\n[" << hRes.first << ", " << hRes.second << "]\n" << tests[i][hRes.first] << ", " << tests[i][hRes.second] << "\n" << hValid;
         }
-        else{
-            hValid = "= " + std::to_string(tars[i]) + "? invalid";
-        }
-
-        std::cout<<"[" << bRes.first << ", " << bRes.second << "]\n" << tests[i][bRes.first] << ", " << tests[i][bRes.second] << "\n" << bValid << 		"\n[" << hRes.first << ", " << hRes.second << "]\n" << tests[i][hRes.first] << ", " << tests[i][hRes.second] << "\n" << hValid << "\n\n";
     }
 }
